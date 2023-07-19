9196eb25d43d131714c10773611743c2d93073da
satyam jha <jhasatyam386@gmail.com>
2023-07-19 16:17:27 +0530

    19july

--- Firebase_New_For/ViewController.swift
+++ Firebase_New_For/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
+    
+    var ref:DatabaseReference!
+    var ref2:Firestore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        ref2 = Firestore.firestore()
+        
     }
 
-
+    @IBAction func submitButtonAction(_ sender: Any) {
+      authodination()
+    }
+    
+    func realtime(){
+       
+    }
+    func refrence(){
+        
+    }
+    func addData(){
+        
+    }
+    func authodination(){
+        Auth.auth().createUser(withEmail: text1.text!, password: text2.text!) { authoDataResult, error in
+            print(authoDataResult,error?.localizedDescription)
+        }
+        
+    }
+    
 }
 
